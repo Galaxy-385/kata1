38a0c9523685fb62947006a01748858c24594b1c
Usuario <vjga2006@gmail.com>
2026-09-24 12:47:16 +0100

    ...

--- src/main/java/software/ulpgc/Main.java
+++ src/main/java/software/ulpgc/Main.java
@@ -1,4 +1,13 @@
 package software.ulpgc;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Student student1 = new Student("pepe", 20);
+
+        System.out.println(student1.getName());
+        System.out.println(student1.getAge());
+        System.out.println(student1.isAdult());
+    }
 }
