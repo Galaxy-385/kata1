92d84efda7908968c005a4b6b7527c7bcf2e7f55
Usuario <vjga2006@gmail.com>
2026-09-24 12:30:06 +0100

    finnish class main and student

--- src/main/java/software/ulpgc/Student.java
+++ src/main/java/software/ulpgc/Student.java
@@ -1,4 +1,24 @@
 package software.ulpgc;
 
 public class Student {
+
+    private String name;
+    private int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isAdult() {
+        return age >= 18;
+    }
 }
